51391ff0ba2e2b27a06db9ff0dc086827fc1d001
dPwr394 <122989115+dPwr394@users.noreply.github.com>
2024-03-25 00:39:31 +0200

    Update marvelapi.js

--- scripts/marvelapi.js
+++ scripts/marvelapi.js
@@ -2,7 +2,7 @@
 const publicKey = 'f5d1f1ec4c6252ff9f5b054433d42ffa';
 const privateKey = 'fdea44270f31ee1eefbc07764be728e08cd3b613';
 const apiBaseURL = "https://gateway.marvel.com//v1/public";
-
+//I FUCKING HATE GUTHUB'S DEPLOY SYSTEM
 // Creates a URL for searching Marvel API for comics with titles starting with a given search term
 function createURL(slash) {
   // Get the current timestamp
