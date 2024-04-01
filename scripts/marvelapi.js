6e1f0e334a8f4508963bdf949e51b34503bd1c9b
dPwr394 <122989115+dPwr394@users.noreply.github.com>
2024-04-01 16:38:42 +0300

    Update marvelapi.js

--- scripts/marvelapi.js
+++ scripts/marvelapi.js
@@ -1,8 +1,8 @@
 
 const publicKey = 'f5d1f1ec4c6252ff9f5b054433d42ffa';
 const privateKey = 'fdea44270f31ee1eefbc07764be728e08cd3b613';
-const apiBaseURL = "http://gateway.marvel.com//v1/public";
-
+const apiBaseURL = "https://gateway.marvel.com//v1/public";
+//FUCK I DID THE SAME MISTAKE TWICE               D O     N O T        D E L E T E      T H E       S     F R O M      T H E    H T T P S      **********EVER**************
 // Creates a URL for searching Marvel API for comics with titles starting with a given search term
 function createURL(slash) {
 
